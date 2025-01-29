cc7128ab02042798d20f2d3185b7b684889cf91a
Devdatt Rupapara <devdattrupapara21@gmail.com>
2025-01-29 22:58:30 +0530

    Fixed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,16 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-}
-  
+  serverExternalPackages: ["mongoose"], // ✅ Fixed
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
 export default nextConfig;
